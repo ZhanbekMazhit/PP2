9601847df718e6083de327dfc9defb69e37cfd52
ZhanibekMazhit <zhanibekmazhit2@gmail.com>
2025-03-12 10:23:12 +0500

    clock

--- lab7/clock.py
+++ lab7/clock.py
@@ -6,9 +6,11 @@ pygame.init()
 screen = pygame.display.set_mode((1400,1000))
 pygame.display.set_caption("Clock")
 clock = pygame.time.Clock()
+test_font = pygame.font.Font(None,50)
 mike = pygame.image.load("C:/Users/zhani/OneDrive/Рабочий стол/pp2/lab7/image/clock.png")
-mike_lefthand=pygame.image.load("C:/Users/zhani/OneDrive/Рабочий стол/pp2/lab7/image/leftarm.png")
-mike_righthand=pygame.image.load("C:/Users/zhani/OneDrive/Рабочий стол/pp2/lab7/image/rightarm.png")
+mike_lefthand=pygame.image.load("C:/Users/zhani/OneDrive/Рабочий стол/pp2/lab7/image/leftarm.png").convert_alpha()
+mike_righthand=pygame.image.load("C:/Users/zhani/OneDrive/Рабочий стол/pp2/lab7/image/rightarm.png").convert_alpha()
+
 RED = (255, 0, 0)
 black = (50,50,5)
 clock_center=(700,525)
@@ -27,17 +29,20 @@ while True:
     now=datetime.datetime.now()
     seconds = now.second + now.microsecond / 1_000_000 
     minutes = now.minute + seconds / 60
-    hours = (now.hour % 12) + minutes / 60
-    minute_angle = minutes * 6  
-    hour_angle = hours * 30
+    second_angle = -seconds * 6
+    minute_angle = -minutes * 6 
+    time = datetime.datetime.now().strftime('%M:%S')
+    text_surface = test_font.render(time,True,'Pink')
+    text_rec = text_surface.get_rect(topleft = (0,0))
     
     screen.blit(mike,(0,0))  
-    pygame.draw.circle(screen, RED, (200,800), 50)
     pygame.draw.circle(screen, black, (700,525), 30)
-    rotated_minute, minute_rect = rotate_image(mike_lefthand, minute_angle, clock_center)
-    rotated_hour, hour_rect = rotate_image(mike_righthand, hour_angle, clock_center)
+    rotated_second, second_rect = rotate_image(mike_lefthand, second_angle, clock_center)
+    rotated_minute, minute_rect = rotate_image(mike_righthand, minute_angle, clock_center)
     screen.blit(rotated_minute, minute_rect.topleft)
-    screen.blit(rotated_hour, hour_rect.topleft)
+    screen.blit(rotated_second, second_rect.topleft)
+    pygame.draw.rect(screen,'black',text_rec,0,5)
+    screen.blit(text_surface,text_rec)
     pygame.display.update()
     clock.tick(60)
     
